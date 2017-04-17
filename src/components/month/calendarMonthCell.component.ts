c886a6875bd46a5859228266070cc4a113adfee2
Brandon R Staley <bstaley0@gmail.com>
2017-04-17 11:35:40 -0400

    chore: dif really messed up

--- src/components/month/calendarMonthCell.component.ts
+++ src/components/month/calendarMonthCell.component.ts
@@ -1,39 +1,13 @@
-import { Component, Input, Output, EventEmitter, TemplateRef } from '@angular/core';
+﻿import { Component, Input, Output, EventEmitter, TemplateRef } from '@angular/core';
 import { MonthViewDay, CalendarEvent } from 'calendar-utils';
 
 @Component({
-<<<<<<< HEAD
     selector: 'mwl-calendar-month-cell',
     template: `
-    <div class="cal-cell-top">
-      <span class="cal-day-badge" *ngIf="day.badgeTotal > 0">{{ day.badgeTotal }}</span>
-      <span class="cal-day-number">{{ day.date | calendarDate:'monthViewDayNumber':locale }}</span>
-    </div>
-    <div class="cal-events">
-      <div
-        class="cal-event"
-        *ngFor="let event of ((day.events.length <= 3)?day.events:day.events.slice(0,2))"
-        [style.backgroundColor]="event.color.primary"
-        [ngClass]="event?.cssClass"
-        mwlDraggable
-        [dropData]="{event: event}"
-        [dragAxis]="{x: event.draggable, y: event.draggable}"
-        (click)="$event.stopPropagation(); eventClicked.emit({event: event})">
-        {{(event.title.length>20)?event.title.substring(0,20) + '...' : event.title}}
-      </div>
-      <div 
-	    class="cal-event" 
-	    style="background-color:transparent; color:rgba(0,0,0,.54);" 
-	    *ngIf="day.events.length > 3">
-	    {{day.events.length - 2 + ' More'}}
-=======
-  selector: 'mwl-calendar-month-cell',
-  template: `
     <ng-template #defaultTemplate>
       <div class="cal-cell-top">
         <span class="cal-day-badge" *ngIf="day.badgeTotal > 0">{{ day.badgeTotal }}</span>
         <span class="cal-day-number">{{ day.date | calendarDate:'monthViewDayNumber':locale }}</span>
->>>>>>> refs/remotes/mattlewis92/master
       </div>
       <div class="cal-events">
         <div
@@ -88,13 +62,9 @@ export class CalendarMonthCellComponent {
 
     @Input() tooltipPlacement: string;
 
-<<<<<<< HEAD
     @Output() highlightDay: EventEmitter<any> = new EventEmitter();
-=======
-  @Input() customTemplate: TemplateRef<any>;
 
-  @Output() highlightDay: EventEmitter<any> = new EventEmitter();
->>>>>>> refs/remotes/mattlewis92/master
+    @Input() customTemplate: TemplateRef<any>;
 
     @Output() unhighlightDay: EventEmitter<any> = new EventEmitter();
 
